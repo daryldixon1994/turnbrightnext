eccc823c72b623d159532856102dc28a44883b6b
daryldixon1994 <kmohamedanouar@gmail.com>
2023-06-16 15:09:52 +0100

    update fan club section

--- src/components/fansClub/FanClub.jsx
+++ src/components/fansClub/FanClub.jsx
@@ -9,12 +9,29 @@ const Container = tw.div`box-border lg:min-h-[1580px] flex flex-col justify-betw
 const Head = tw.div`flex items-center justify-center gap-[5px] mx-auto`;
 const Line1 = tw.div`lg:h-[2.5px] lg:w-[830px] bg-[#001436]`;
 const Line2 = tw.div`lg:h-[2.5px] lg:w-[240px] bg-[#001436]`;
-const Media = tw.div`box-border flex justify-between`;
+const TeamSlides = tw.div``;
+const Media = tw.div`box-border flex justify-between lg:mt-8 lg:mb-16`;
+const Item = tw.div`box-border relative lg:w-[233px] flex flex-col items-center gap-2`;
+const ItemImg = tw.div`box-border lg:w-[200px] lg:h-[354px]`;
+const Square = tw.div`box-border border-2 border-black w-[100%] h-[300px] absolute top-[10%]`;
+const Title = tw.div`box-border`;
+const Rectangle = tw.div`w-20 h-10 bg-[#FECB36] mb-[-1.4rem]`;
+const H2 = tw.h2`font-poppins font-extrabold text-[#001436] text-4xl uppercase`;
+const H3 = tw(H2)`text-xl font-normal`;
+const H5 = tw(H2)`text-xl`;
 const Box = tw.div`bg-[#FECB36] text-[#001436] w-[100%] lg:min-h-[350px] relative flex flex-col items-center justify-center gap-12 py-32`;
 const Circle = tw.div`box-border rounded-full p-2 bg-white w-[150px] h-[150px] absolute bottom-[-6%] left-[-3%]`;
 const H1 = tw.h1`font-bold text-6xl font-poppins uppercase`;
 const P = tw.p`font-semibold text-xl font-poppins uppercase w-[55%] text-center leading-loose`;
 function FanClub() {
+  const mediaImgs = [
+    { name: "Yonis", pic: "/images/fan-img1.png" },
+    { name: "Sarra", pic: "/images/fan-img2.png" },
+    { name: "Richard", pic: "/images/fan-img3.png" },
+    { name: "Fred", pic: "/images/fan-img4.png" },
+    { name: "Elodie", pic: "/images/fan-img5.png" },
+  ];
+
   return (
     <Container>
       <Head>
@@ -22,11 +39,26 @@ function FanClub() {
         <Header1 title1="Fan's" title2="Club" />
         <Line2 />
       </Head>
-      <Media>
-        <Image src="/images/fan-img-1.png" alt="" width={350} height={350} />
-        {/* <Image src="/images/fan-img-2.png" alt="" width={350} height={350} /> */}
-        <Image src="/images/fan-img-3.png" alt="" width={350} height={350} />
-      </Media>
+      <TeamSlides>
+        <Title>
+          <Rectangle />
+          <H2>TEAM SLIDES</H2>
+        </Title>
+        <H3>Unleashing the Power of Collaboration</H3>
+        <Media>
+          {mediaImgs.map((img, i) => (
+            <Item key={i}>
+              <ItemImg
+                style={{ backgroundImage: `url(${img.pic})` }}
+                className="img-container"
+              />
+              <Square />
+              <H5> {img.name} </H5>
+            </Item>
+          ))}
+          {/* <Image src="/images/fan-img-1.png" alt="" width={350} height={350} /> */}
+        </Media>
+      </TeamSlides>
       <Box>
         <H1>Turn B. Right </H1>
         <P>
